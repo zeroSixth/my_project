3a5071ab3bc9617c4cb14d776ea430ccc3a6d567
ZeroSixth <lado.belkaniya@gmail.com>
2024-02-21 14:13:31 +0300

    Add function to capitalize words and update docstring

--- str_func.py
+++ str_func.py
@@ -1,3 +1,6 @@
 def to_upper_case(s):
     """Convert a string to uppercase."""
     return s.upper()
+def capitalize_words(s):
+    """Capitalize the first letter of each word in a string."""
+    return ' '.join(word.capitalize() for word in s.split())
